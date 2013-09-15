beffe01f4bd87ef62ac377c6b62c731e3c8c57b6
never <never.wencan@gmail.com>
2013-09-16 00:02:57 +0800

    The eye can work now, but need further beautify

--- Eye/eye.py
+++ Eye/eye.py
@@ -10,16 +10,16 @@ from PIL import Image, ImageTk
 import ctypes
 
 '''state can be "rest", "wait_work", "work"'''
-state = "rest"
-#REST_TIME = datatime.timedelta(minutes = 10)
-REST_TIME = datetime.timedelta(seconds = 1)
-WORK_TIME = datetime.timedelta(seconds = 5)
+state = "wait_work"
+#REST_TIME = datetime.timedelta(seconds = 10)
+REST_TIME = datetime.timedelta(minutes = 10)
+WORK_TIME = datetime.timedelta(minutes = 50)
+#WORK_TIME = datetime.timedelta(seconds = 10)
 
 class Window(Tkinter.Frame):
     def __init__(self):
         monitor_width = ctypes.windll.user32.GetSystemMetrics(0)
         monitor_height = ctypes.windll.user32.GetSystemMetrics(1)
-        print monitor_height, monitor_width
         Tkinter.Frame.__init__(self, width = monitor_width, height = monitor_height)
         '''removes all window manager decorations from the window'''
         self.winfo_toplevel().overrideredirect(True)
@@ -47,45 +47,65 @@ class Window(Tkinter.Frame):
         self.time.grid(sticky = Tkinter.N)
         '''set the width of cell 0, which place the time'''
         self.columnconfigure(0, weight = monitor_width)
-        self.tick()
         
         '''add a start work button'''
-        start_work = Tkinter.Button(self, text = "Start Work !", command = self.__start_work);
-        start_work.grid()
+        self.work = Tkinter.Button(self, text = "Start Work !", command = self.__start_work, 
+                                   font = ('Times', '30'));
         '''set the width and height of cell 0, which place the button'''
         self.columnconfigure(0, weight = monitor_width)
         self.rowconfigure(1, weight = monitor_height)
-    
-    def __start_work(self):
+        
+        self.set_work_state()
+        
+        self.tick()
+        
+    def set_rest_state(self):     
+        self.state = 'rest'
+        self.start_time = datetime.datetime.now()
+        self.winfo_toplevel().deiconify()    
+        
+    def set_wait_work_state(self):
+        self.state = 'wait_work'
+        self.work.grid()
+        self.winfo_toplevel().deiconify()
+        
+    def set_work_state(self): 
+        self.state = 'work'
+        self.start_time = datetime.datetime.now()
         self.winfo_toplevel().withdraw()
+        self.work.grid_remove()
+        
+    def __start_work(self):
+        self.set_work_state()
         
     def format_timedelta(self, time_delta):
         day = str(time_delta.days)
         hour = str(time_delta.seconds // 3600)
         min = str((time_delta.seconds // 60) % 60) 
         sec = str(time_delta.seconds % 60)
-        return (day.rjust(2, '0') + ":" + hour.rjust(2, '0') + ":"  + min.rjust(2, '0')+ ":" + sec.rjust(2, '0'))
+        return (day.rjust(2, '0') + ":" + hour.rjust(2, '0') + ":"  + min.rjust(2, '0')+ 
+                ":" + sec.rjust(2, '0'))
         
     def tick(self):
+        global REST_TIME, WORK_TIME
+        
         cur_time = datetime.datetime.now()
-        time_delta = cur_time - start_time
+        time_delta = cur_time - self.start_time 
         
-        global state, REST_TIME, WORK_TIME
-        
-        if state == 'rest':
+        if self.state == 'rest':
             if time_delta > REST_TIME :
-                state == 'wait_work'
-        elif state == 'work':
-            if time_delta > WORK_TIME :
-                state = 'rest'    
+                self.set_wait_work_state()
+        elif self.state == 'work':
+            if time_delta > WORK_TIME : 
+                self.set_rest_state()
         
         '''show time'''
         self.time.after(1000, self.tick)
+        time_delta = cur_time - self.start_time
         self.time.config(text = self.format_timedelta(time_delta))
         
 
 if __name__ == '__main__':
-    start_time = datetime.datetime.now()
     window = Window()
     window.mainloop()
         
