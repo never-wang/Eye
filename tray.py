5853d6473ffaf0a8bd32e7172250469b37020126
Wang Wencan <never.wencan@gmail.com>
2013-10-18 17:08:58 +0800

    1. add linux config window

--- tray.py
+++ tray.py
@@ -101,7 +101,7 @@ class LinuxTray(Tray):
         self.tray.set_icon(os.path.join(os.path.abspath(os.path.dirname(__file__)), "eye.gif"))
         self.tray.set_attention_icon('indicator-messages-new')
         self.tray.set_menu(self.make_menu())
-    
+
     def make_menu(self):
         menu = gtk.Menu()
         for id, text, callback in self.itemlist:
@@ -114,40 +114,51 @@ class LinuxTray(Tray):
     
     def menu_config(self, event):
         '''user config the Eye by a window, the config will be stored in config file too'''
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.set_default
-        self.config_frame = wx.Frame(None, size = (300, 300), title = '设置',
-                                     style = wx.DEFAULT_FRAME_STYLE & (~ wx.RESIZE_BORDER) & (~ wx.MAXIMIZE_BOX))
-        self.config_frame.SetBackgroundColour(wx.ColourDatabase().Find("WHITE"))
-        self.config_frame.Centre()
-        self.config_frame.Show()
-        frame_width, frame_height = self.config_frame.GetSize()
-        
-        box = wx.BoxSizer(wx.VERTICAL)
-        self.config_frame.SetSizer(box)
+        self.config_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.config_window.set_size_request(
+                self.config_window_width,
+                self.config_window_height)
+        self.config_window.set_resizable(True)
+        self.config_window.set_position(gtk.WIN_POS_CENTER_ALWAYS)
+        self.config_window.set_resizable(False)
 
-        self.config_text = wx.TextCtrl(self.config_frame, style = wx.TE_MULTILINE)
-        text = self.view.config.text()
-        if text != None:
-            self.config_text.WriteText(text)
-        box.Add(self.config_text, 1, wx.EXPAND | wx.ALL, 0)
-        
-        button_box = wx.BoxSizer(wx.HORIZONTAL)
-        box.Add(button_box, 0)
+        box = gtk.VBox(False, 0)
+        self.config_window.add(box)
 
-        accept_button = wx.Button(self.config_frame, label = '确定')
-        accept_button_width, accept_button_height = accept_button.GetSize()
-        accept_button.Bind(wx.EVT_BUTTON, self.config_accept)
-        button_box.Add(accept_button, 0, wx.LEFT, frame_width / 2 - accept_button_width - 5)
-        cancel_button = wx.Button(self.config_frame, label = '取消')
-        cancel_button_width, cancel_button_height = accept_button.GetSize()
-        cancel_button.Bind(wx.EVT_BUTTON, self.config_cancel)
-        button_box.Add(cancel_button, 0, wx.RIGHT, frame_width / 2 - cancel_button_width - 5)
-        self.config_frame.Layout()       
+        text_buffer=gtk.TextBuffer()
+        text_buffer.set_text(self.view.config.text())
+
+        self.text_view = gtk.TextView(text_buffer)
+        box.pack_start(self.text_view)
+
+        button_table = gtk.Table(rows = 1, columns = 4, homogeneous = True)
+        box.pack_start(button_table, False, False, 0)
+
+        accept_button = gtk.Button(label = '确定')
+        button_table.attach(accept_button, 1, 2, 0, 1)
+        accept_button.connect('clicked', self.config_accept)
+        
+        cancel_button = gtk.Button(label = '取消')
+        button_table.attach(cancel_button, 2, 3, 0, 1)
+        cancel_button.connect('clicked', self.config_cancel)
+        
+        self.config_window.show_all()
+
+    def config_accept(self, event):
+        text_buffer = self.text_view.get_buffer()
+        text = text_buffer.get_text(text_buffer.get_start_iter(),
+                text_buffer.get_end_iter())
+        self.view.config.set_text(text)
+        self.config_window.hide_all()
+        del self.config_window
+        del self.text_view
+
+    def config_cancel(self, event):
+        self.config_window.hide_all()
+        del self.config_window
+        del self.text_view
+
 
     def menu_quit(self, event):
         gtk.main_quit()
         
-
-if __name__ == "__main__":
-    Tray("test")
